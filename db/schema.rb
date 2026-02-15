3aeb4b2382a5e4161ee1ff99fa9c023a010b4d24
Vincent Di Nicolantonio <vincentdinic@gmail.com>
2026-02-15 12:32:40 -0500

    Migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_15_161546) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_15_171445) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "families", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "family_users", id: false, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "family_id", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["family_id"], name: "index_family_users_on_family_id"
+    t.index ["user_id", "family_id"], name: "index_family_users_on_user_id_and_family_id", unique: true
+    t.index ["user_id"], name: "index_family_users_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.date "birthday", null: false
@@ -29,4 +45,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_15_161546) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "family_users", "families"
+  add_foreign_key "family_users", "users"
 end
