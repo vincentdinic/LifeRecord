432c29edfc5f8311a4c56388cbca7089aedc97aa
Vincent Di Nicolantonio <vincentdinic@gmail.com>
2026-04-22 10:54:46 -0400

    Add images to vehicles and properties show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   # Determines if the nav bar link should be highlighted based on current page
   def nav_link_class(path)
-    current_page?(path) ? "nav-link text-info" : "nav-link"
+    current_page?(path) ? "nav-link text-info fw-bold" : "nav-link"
   end
 end
