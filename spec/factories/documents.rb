35271fcc07e0eb5f55d7214db3561582eec67ff1
Vincent Di Nicolantonio <vincentdinic@gmail.com>
2026-04-24 13:50:01 -0400

    List view for calendar & seeder changes

--- spec/factories/documents.rb
+++ spec/factories/documents.rb
@@ -3,5 +3,7 @@ FactoryBot.define do
     title         { Faker::Book.title }
     description   { Faker::Lorem.sentence if [ true, false ].sample }
     document_type { Document.document_types.keys.sample }
+    created_at { Faker::Date.backward(days: 3650) }
+    updated_at { Faker::Date.backward(days: 250) }
   end
 end
